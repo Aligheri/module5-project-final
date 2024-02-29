4ff15190d56bd351dddf97c394823ccf7348edc7
Aligheri <ivanyevsieiev@gmail.com>
2024-02-29 13:52:36 +0200

    localization in two languages was added for letter templates (mails) and the index.html start page.

--- src/main/java/com/javarush/jira/common/internal/config/ThymeleafConfig.java
+++ src/main/java/com/javarush/jira/common/internal/config/ThymeleafConfig.java
@@ -1,12 +1,17 @@
 package com.javarush.jira.common.internal.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import org.thymeleaf.spring6.SpringTemplateEngine;
 import org.thymeleaf.templatemode.TemplateMode;
 import org.thymeleaf.templateresolver.FileTemplateResolver;
 
+import java.util.Locale;
 import java.util.Set;
 
 @Configuration
@@ -25,6 +30,7 @@ public class ThymeleafConfig {
         FileTemplateResolver mailResolver = createTemplateResolver("./resources/mails/");
         mailResolver.setOrder(2);
         engine.setTemplateResolvers(Set.of(viewResolver, mailResolver));
+//        engine.setEnableSpringELCompiler(true);
         return engine;
     }
 
@@ -37,5 +43,19 @@ public class ThymeleafConfig {
             setCacheTTLMs(appProperties.getTemplatesUpdateCache().toMillis());
             setCharacterEncoding("UTF-8");
         }};
+    }
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasenames("classpath:/messages/messages", "classpath:/messages/email");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(Locale.US);
+        return slr;
     }
 }
